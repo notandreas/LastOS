75476f1ac46bcce35b1cd640a5ca3e42a550ae68
notandreas <notandreas@mailbox.tu-berlin.de>
2021-12-18 14:54:18 +0100

    tcb with status

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -1,15 +1,41 @@
 #include <kernel/kthread.h>
 
-typedef struct _kthread_save {
-    arm_registers save;
-    const void *arg;
-    unsigned int args_s;
-} kthread_save;
+typedef enum _thread_status {
+    READY,
+    RUNNING,
+    WAITING,
+    TERMINATED
+} thread_status;
 
-static kthread_save thread_list[THREAD_COUNT];
+
+typedef struct _tcb {
+    uint32_t r0;
+    uint32_t r1;
+    uint32_t r2;
+    uint32_t r3;
+    uint32_t r4;
+    uint32_t r5;
+    uint32_t r6;
+    uint32_t r7;
+    uint32_t r8;
+    uint32_t r9;
+    uint32_t r10;
+    uint32_t r11;
+    uint32_t r12;
+    uint32_t sp;
+    uint32_t lr;
+    uint32_t pc;
+    uint32_t cpsr;
+    thread_status t_status;
+} tcb;
+
+static tcb thread_list[THREAD_COUNT];
+static int tcb_current_thread = -1;
+
 
 void kthread_start(int thread_id) {
 }
+
 
 void kthread_create(void (*func)(void*), const void *args, unsigned int args_size){
 }
